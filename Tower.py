e7a7545bb168786f63649d6f1e5790377c47ca27
Brandon Zupan <zupan.brandon@utexas.edu>
2019-10-29 16:15:39 -0500

    Add set_image() testing

---
+++ Tower.py
@@ -0,0 +1,26 @@
+#Brandon Zupan
+#Class for UT Tower
+
+import requests
+from PIL import Image
+from io import BytesIO
+
+class Tower:
+    def __init__(self):
+        #Run aquire image method to get one on init
+        self.image = None
+        self.top_color = None
+        self.base_color = None
+        self.URL = "http://wwc.instacam.com/instacaming/UTAUS/UTAUS_l.jpg"
+
+    def set_image(self):
+        """Returns an image object for further analysis"""
+        img_data = requests.get(self.URL)
+        tower_image = Image.open(self.URL)
+        img.show()
+
+        #
+
+tower = Tower()
+
+tower.set_image()
